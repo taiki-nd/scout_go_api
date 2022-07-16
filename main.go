eaba1d9d111f9856cf68dbbef485a03a35d5bb5c
taiki-nd <kii.develop.0517@gmail.com>
2022-07-16 23:15:46 +0900

    ログ出力のテスト

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/taiki-nd/scout_go_api/config"
+	"github.com/taiki-nd/scout_go_api/utils"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	utils.Logging(config.Config.LogFile)
+
+	log.Println("test_log")
 }
