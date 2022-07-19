c5d5bf3b727bbd4ac21790830f4422c0356d57e2
taiki-nd <kii.develop.0517@gmail.com>
2022-07-19 20:40:33 +0900

    cors設定

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,9 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/taiki-nd/scout_go_api/controllers"
 )
 
 func Routes(app *fiber.App) {
-	app.Use(cors.New(cors.Config{
-		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
-		AllowHeaders: "Content-Type, Content-Length, Authorization, Uid",
-		AllowOrigins: "*",
-	}))
 	app.Get("/", controllers.UsersIndex)
 }
