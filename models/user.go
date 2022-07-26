bc37f85e1f468532f08387af0debaf97e4b79c77
taiki-nd <kii.develop.0517@gmail.com>
2022-07-26 21:00:06 +0900

    userモデルの修正

--- models/user.go
+++ models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	Id             uint      `json:"id gorm:"primaryKey"`
+	Id             uint      `json:"id" gorm:"primaryKey"`
 	Uuid           string    `json:"uuid" gorm:"not null; size:256"`
 	LastName       string    `json:"last_name" gorm:"not null; size:256"`
 	LastNameKana   string    `json:"last_name_kana" gorm:"not null; size:256"`
-	FirstName      string    `json:"first_kame" gorm:"not null; size:256"`
+	FirstName      string    `json:"first_name" gorm:"not null; size:256"`
 	FirstNameKana  string    `json:"first_name_kana" gorm:"not null; size:256"`
 	Nickname       string    `json:"nickname" gorm:"not null; size:256"`
 	Sex            string    `json:"sex" gorm:"not null; size:256"`
