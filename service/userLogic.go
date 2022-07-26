d5cdc00c97ff25d66865f9d2fc58b8e99cd7f5bc
taiki-nd <kii.develop.0517@gmail.com>
2022-07-26 22:35:54 +0900

    レコード取得方法の変更

--- service/userLogic.go
+++ service/userLogic.go
@@ -4,6 +4,7 @@ import (
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/taiki-nd/scout_go_api/db"
 	"github.com/taiki-nd/scout_go_api/models"
 )
 
@@ -12,10 +13,12 @@ import (
  * idからuser詳細情報を取得
  * @return models.User
  */
-func GetUserFromId(c *fiber.Ctx) models.User {
+func GetUserFromId(c *fiber.Ctx) (models.User, error) {
 	id, _ := strconv.Atoi(c.Params("id"))
-	user := models.User{
-		Id: uint(id),
+	var user models.User
+	err := db.DB.Where("id", id).First(&user).Error
+	if err != nil {
+		return user, err
 	}
-	return user
+	return user, nil
 }
