962b22b13d5b6de1682ca3c700f7da7ca5895053
taiki-nd <kii.develop.0517@gmail.com>
2022-07-29 00:31:38 +0900

    アソシエーションモデルの修正

--- models/userAssociation.go
+++ models/userAssociation.go
@@ -1,18 +1,18 @@
 package models
 
 type UserAssociation struct {
-	Uuid           string
-	LastName       string
-	LastNameKana   string
-	FirstName      string
-	FirstNameKana  string
-	Nickname       string
-	Sex            string
-	BirthYear      int
-	BirthMonth     int
-	BirthDay       int
-	AutoPermission bool
-	IsExample      bool
-	IsAdmin        bool
-	Statuses       []int
+	Uuid           string `json:"uuid" gorm:"not null; size:256"`
+	LastName       string `json:"last_name" gorm:"not null; size:256"`
+	LastNameKana   string `json:"last_name_kana" gorm:"not null; size:256"`
+	FirstName      string `json:"first_name" gorm:"not null; size:256"`
+	FirstNameKana  string `json:"first_name_kana" gorm:"not null; size:256"`
+	Nickname       string `json:"nickname" gorm:"not null; size:256"`
+	Sex            string `json:"sex" gorm:"not null; size:256"`
+	BirthYear      int    `json:"birth_year" gorm:"not null"`
+	BirthMonth     int    `json:"birth_month" gorm:"not null"`
+	BirthDay       int    `json:"birth_day" gorm:"not null"`
+	AutoPermission bool   `json:"auto_permission" gorm:"not null; default:false"`
+	IsExample      bool   `json:"is_example" gorm:"not null; default:false"`
+	IsAdmin        bool   `json:"is_admin" gorm:"not null; default:false"`
+	Statuses       []int  `json:"statuses" gorm:"many2many:user_statuses"`
 }
