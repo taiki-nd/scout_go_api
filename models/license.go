70005f3d44e22e418f936046348e77893fea4eae
taiki-nd <kii.develop.0517@gmail.com>
2022-07-30 15:51:22 +0900

    licenseテーブルの作成

---
+++ models/license.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Licenses struct {
+	Id        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"not null; size:256"`
+	GetYear   int       `json:"get_year" gorm:"not null"`
+	GetMonth  int       `json:"get_month" gorm:"not null"`
+	Public    bool      `json:"public" gorm:"not null; default:false"`
+	Publish   bool      `json:"publish" gorm:"not null; default:false"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at`
+}
