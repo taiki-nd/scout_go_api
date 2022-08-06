cb0f32c4c5d36809fad4066cda8ab0a082864e6e
taiki-nd <kii.develop.0517@gmail.com>
2022-08-06 20:07:00 +0900

    アソシエーションの修正

--- db/connect.go
+++ db/connect.go
@@ -32,13 +32,13 @@ func ConnectToDb() {
 	log.Printf("success db connection: %v", db)
 
 	db.AutoMigrate(
+		&models.User{},
+		&models.Work{},
 		&models.Activity{},
 		&models.License{},
 		&models.Prefecture{},
 		&models.School{},
 		&models.Status{},
 		&models.Project{},
-		&models.Work{},
-		&models.User{},
 	)
 }
